d44c4bf611265b70c3871d107b0b59ad7a1af2ac
Filip Duda <dudafilip80@seznam.cz>
2021-06-25 20:04:21 +0200

    added boilerplate and langingpage

---
+++ src/boilerplate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// third party 
+// pages
+// components
+// styles
+import 'boilerplate.sass';
+
+const boilerplate = () => {
+    return (
+        <div>boilerplate</div>
+    );
+};
+
+export default boilerplate;
---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// third party 
+// pages
+// components
+// styles
+import 'LandingPage.sass';
+
+const LandingPage = () => {
+    return (
+        <div>LandingPage</div>
+    );
+};
+
+export default LandingPage;
